c2f51a935ac092de0cba24000fd67c379ab524f3
Abdul Waajid Ahmed <106549884+Wajidmk@users.noreply.github.com>
2024-07-02 21:28:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let saveEl = document.getElementById("save-el");
+let countEl = document.getElementById("count-el")// pass in argument 
+let count = 0 
+
+
+
+
+ function increment(){
+    count   += 1
+   countEl.innerText = count 
+ }
+
+
+
+function save (){
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+}
+ 
+
+
+
+
+
